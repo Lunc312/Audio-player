4e67ca0675ab8c756fbd4cc6a4d6c64292781c67
Vladimir Samorodov <vovchikan@gmail.com>
2019-07-09 19:27:59 +0500

    New design, fix README

--- MainWindow.py
+++ MainWindow.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'd:\plena\Audio-player\MainWindow.ui'
+# Form implementation generated from reading ui file 'd:\Programming\Python\Qt5\audio-player\MainWindow.ui'
 #
 # Created by: PyQt5 UI code generator 5.11.3
 #
@@ -11,10 +11,41 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(670, 397)
-        self.pushButton = QtWidgets.QPushButton(Dialog)
-        self.pushButton.setGeometry(QtCore.QRect(40, 50, 75, 23))
-        self.pushButton.setObjectName("pushButton")
+        Dialog.resize(729, 484)
+        Dialog.setSizeGripEnabled(False)
+        self.gridLayout = QtWidgets.QGridLayout(Dialog)
+        self.gridLayout.setObjectName("gridLayout")
+        self.listView_songs = QtWidgets.QListView(Dialog)
+        self.listView_songs.setObjectName("listView_songs")
+        self.gridLayout.addWidget(self.listView_songs, 1, 1, 1, 1, QtCore.Qt.AlignRight)
+        self.label = QtWidgets.QLabel(Dialog)
+        font = QtGui.QFont()
+        font.setFamily("Century Gothic")
+        font.setPointSize(30)
+        font.setBold(True)
+        font.setItalic(False)
+        font.setUnderline(True)
+        font.setWeight(75)
+        font.setStrikeOut(False)
+        self.label.setFont(font)
+        self.label.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName("label")
+        self.gridLayout.addWidget(self.label, 1, 0, 1, 1)
+        self.horizontalLayout_topMenu = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_topMenu.setObjectName("horizontalLayout_topMenu")
+        self.pushButton_openf = QtWidgets.QPushButton(Dialog)
+        self.pushButton_openf.setObjectName("pushButton_openf")
+        self.horizontalLayout_topMenu.addWidget(self.pushButton_openf)
+        self.pushButton_2 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.horizontalLayout_topMenu.addWidget(self.pushButton_2)
+        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_topMenu.addItem(spacerItem)
+        self.pushButton_3 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.horizontalLayout_topMenu.addWidget(self.pushButton_3)
+        self.gridLayout.addLayout(self.horizontalLayout_topMenu, 0, 0, 1, 2)
 
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
@@ -22,5 +53,8 @@ class Ui_Dialog(object):
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.pushButton.setText(_translate("Dialog", "PushButton"))
+        self.label.setText(_translate("Dialog", "Тут будет картинка альбома"))
+        self.pushButton_openf.setText(_translate("Dialog", "Открыть папку"))
+        self.pushButton_2.setText(_translate("Dialog", "PushButton"))
+        self.pushButton_3.setText(_translate("Dialog", "PushButton"))
 
--- main.py
+++ main.py
@@ -1,19 +1,33 @@
-from MainWindow import QtWidgets, Ui_Dialog
+from PyQt5 import QtWidgets
+from MainWindow import Ui_Dialog
+import sys
 
 
 def qq():
     print('Hello World')
 
+class mywindow(QtWidgets.QDialog):
+    def __init__(self):
+        super(mywindow, self).__init__()
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
 
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    MyMainWindow = QtWidgets.QMainWindow()
-    ui = Ui_Dialog()
-    ui.setupUi(MyMainWindow)
-    ui.pushButton.clicked.connect(qq)
-    MyMainWindow.show()
-    sys.exit(app.exec_())
+
+app = QtWidgets.QApplication([])
+application = mywindow()
+application.ui.pushButton_openf.clicked.connect(qq)
+application.show()
+
+sys.exit(app.exec())
+# if __name__ == "__main__":
+#     import sys
+#     app = QtWidgets.QApplication(sys.argv)
+#     MyMainWindow = QtWidgets.QMainWindow()
+#     ui = Ui_Dialog()
+#     ui.setupUi(MyMainWindow)
+#     ui.pushButton_openf.clicked.connect(qq)
+#     MyMainWindow.show()
+#     sys.exit(app.exec_())
 
 
 
