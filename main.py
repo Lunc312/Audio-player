0f752fb7c27ee741fde0e1f5d6b79fbeea1ce935
Vladimir Samorodov <vovchikan@gmail.com>
2019-07-09 21:12:26 +0500

    Getting directory by QFileDialog

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ from PyQt5 import QtWidgets
 from MainWindow import Ui_Dialog
 import sys
 
+import songs
 
-def qq():
-    print('Hello World')
 
 class mywindow(QtWidgets.QDialog):
     def __init__(self):
@@ -12,10 +11,15 @@ class mywindow(QtWidgets.QDialog):
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
 
+    def show_dialog(self):
+        filename = str(QtWidgets.QFileDialog.getExistingDirectory(self, "Выберите папку с музыкой"))
+
+        print(songs.get_songs_list(filename))
+
 
 app = QtWidgets.QApplication([])
 application = mywindow()
-application.ui.pushButton_openf.clicked.connect(qq)
+application.ui.pushButton_openf.clicked.connect(application.show_dialog)
 application.show()
 
 sys.exit(app.exec())
